89be59552c7d854638c6af6875f26119258382e0
Eric Julius <ejulius86@gmail.com>
2018-02-15 17:16:53 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,13 @@
+const filter = require('./lib/filter');
+const findIndex = require('./lib/findIndex');
+const forEach = require('./lib/forEach');
+const map = require('./lib/map');
+const reduce = require('./lib/reduce');
+
+module.exports = {
+  filter,
+  findIndex,
+  forEach,
+  map,
+  reduce
+};
